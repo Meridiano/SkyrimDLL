e999d19c20102827fb40f4a276e1b2a124e080a2
Meridiano <megametallist@gmail.com>
2025-07-12 21:44:14 +0300

    FixDualWieldParrying 1.0.0 Minor Tweaks

--- FixDualWieldParrying/src/main.cpp
+++ FixDualWieldParrying/src/main.cpp
@@ -1,6 +1,6 @@
 std::string CalculateHash(fs::path path) {
 	if (fs::exists(path) && fs::is_regular_file(path)) {
-		std::ifstream stream(path, std::ios::in + std::ios::binary);
+		std::ifstream stream(path, std::ios::binary);
 		std::vector<std::uint8_t> hash(picosha2::k_digest_size);
 		picosha2::hash256(stream, hash.begin(), hash.end());
 		return picosha2::bytes_to_hex_string(hash);
@@ -20,8 +20,7 @@ void MessageListener(SKSE::MessagingInterface::Message* msg) {
 		bool ngPatched = false;
 		bool ogPatched = false;
 		// case 1 - ng version
-		auto ngModule = FindModule(DualWieldParryingNG);
-		if (ngModule) {
+		if (auto ngModule = FindModule(DualWieldParryingNG); ngModule) {
 			auto sha256 = CalculateHash("Data/SKSE/Plugins/DualWieldParryingNG.dll");
 			bool match = HashEqual(sha256, "4a401832814a043009f9ca7cc724da0531674a10556c3f4aa98292df19dc6ed2");
 			if (match) {
@@ -30,8 +29,7 @@ void MessageListener(SKSE::MessagingInterface::Message* msg) {
 			} else logs::info("NG version found but no match");
 		} else logs::info("NG version not found");
 		// case 2 - og version
-		auto ogModule = FindModule(DualWieldParryingSKSE);
-		if (ogModule) {
+		if (auto ogModule = FindModule(DualWieldParryingSKSE); ogModule) {
 			auto sha256 = CalculateHash("Data/SKSE/Plugins/DualWieldParryingSKSE.dll");
 			bool match = HashEqual(sha256, "584f6f32d5a8bc941220486c5d64653cd1a193d2903b04e9ba6e40506289964a");
 			if (match) {
